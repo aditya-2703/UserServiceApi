75fa9b949e395bfcabeb8629c8177ae1934c8f45
aditya <adityachauhan2703@gmail.com>
2025-01-24 14:58:40 +0530

    Adding Auth2.0 functionality

--- src/main/java/com/ideabazaar/user/userserviceapi/config/RestTemplateConfig.java
+++ src/main/java/com/ideabazaar/user/userserviceapi/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.ideabazaar.user.userserviceapi.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
 }
